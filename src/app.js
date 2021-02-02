e7011b142bed35bd4ba8f15d446376cfad718090
alexgonczaruk <55724886+alexgonczaruk@users.noreply.github.com>
2021-02-01 22:37:38 -0500

    added express rough backend frame

---
+++ src/app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3002
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('homepage')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/consignment', (req, res) => {
+    res.render('consignment')
+})
+
+app.get('/meetUs', (req, res) => {
+    res.render('devs')
+})
+
+app.get('/productDetails', (req, res) => {
+    res.render('productDetails')
+})
+
+app.get('/sale', (req, res) => {
+    res.render('sale')
+})
+
+app.get('/help', (req, res) => {
+    res.send('its really not that hard bro its just a website lmao')
+})
+
+app.get('/help/*', (req, res) => {
+    res.send('fam how you gonna screw up tryna get to the help page...')
+})
+
+app.get('*', (req, res) => {
+    res.send('page no exist big man')
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
